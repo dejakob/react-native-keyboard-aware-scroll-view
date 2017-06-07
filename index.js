7bd552a1666838d9ea4a2539d7b9389606a793f3
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2017-06-07 14:20:47 +0200

    animated export

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 import KeyboardAwareMixin from './lib/KeyboardAwareMixin'
 import KeyboardAwareScrollView from './lib/KeyboardAwareScrollView'
-import AnimatedKeyboardAwareScrollView from './lib/KeyboardAwareScrollView'
+import AnimatedKeyboardAwareScrollView from './lib/AnimatedKeyboardAwareScrollView'
 import KeyboardAwareListView from './lib/KeyboardAwareListView'
 
 export {
